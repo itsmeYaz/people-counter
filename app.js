075f779e85b3e316bace7491087e1cdcd8f41641
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-07 16:44:53 +0800

    Add increment functionality to count-el

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // document.getElementById("count").innerText = 5;
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById("count");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
