316502791bc879c065662b35d36246c7440d4c14
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-09 17:54:24 +0800

    Add save functionality and reset count

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-// document.getElementById("count").innerText = 5;
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count");
 let count = 0;
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
+}
+
+function save() {
+  let prevCount = ` ${count} - `;
+  saveEl.textContent += prevCount;
+  //My solution reseting the count into zero
+  // let resetCount = count - count;
+  // count = resetCount;
+  // countEl.textContent = count;
+
+  //Course Soulution
+  countEl.textContent = 0;
+  count = 0;
 }
